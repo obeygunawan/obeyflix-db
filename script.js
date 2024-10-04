d701a856f6e0e74e579687b53684d1a0e320c9b0
obeygunawan26 <147344829+obeygunawan26@users.noreply.github.com>
2024-10-05 02:08:46 +0700

    Update index and script for ObeyFlix

--- script.js
+++ script.js
@@ -1,10 +1,57 @@
 // Menangani klik pada tombol Search
-$('.search-button').on('click', function () {
+// $('.search-button').on('click', function () {
+//     performSearch();
+// });
+
+// Menangani penekanan tombol Enter pada kolom pencarian
+// $('.input-keyword').on('keypress', function (event) {
+//     if (event.which === 13) { // Cek jika tombol Enter ditekan
+//         performSearch();
+//     }
+// });
+
+// Fungsi untuk melakukan pencarian
+// function performSearch() {
+//     $.ajax({
+//         url: 'https://www.omdbapi.com/?apikey=437432c&s=' + $('.input-keyword').val(),
+//         success: results => {
+//             const movies = results.Search;
+//             let cards = ''; 
+//             movies.forEach(m => {
+//                 cards += showCards(m);
+//             });
+//             $('.movie-container').html(cards);
+
+//             // Menangani klik pada tombol Show Details
+//             $('.show-details').on('click', function (event) {
+//                 event.preventDefault(); // Mencegah aksi default tautan
+//                 const imdbID = $(this).data('imdbid'); // Perbaiki pengambilan data-imdbid
+//                 $.ajax({
+//                     url: 'https://www.omdbapi.com/?apikey=437432c&i=' + imdbID,
+//                     success: m => {
+//                         const movieDetail = showMovieDetail(m);
+//                         $('.modal-body').html(movieDetail);
+//                         $('#movieDetailModal').modal('show'); // Tampilkan modal
+//                     }
+//                 });
+//             });
+//         },
+//         error: (e) => {
+//             console.log(e.responseText);
+//         }
+//     });
+// }
+
+// Fetch
+// Menangani klik pada tombol Search
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', function () {
     performSearch();
 });
 
 // Menangani penekanan tombol Enter pada kolom pencarian
-$('.input-keyword').on('keypress', function (event) {
+const inputKeyword = document.querySelector('.input-keyword');
+inputKeyword.addEventListener('keypress', function (event) {
     if (event.which === 13) { // Cek jika tombol Enter ditekan
         performSearch();
     }
@@ -12,34 +59,43 @@ $('.input-keyword').on('keypress', function (event) {
 
 // Fungsi untuk melakukan pencarian
 function performSearch() {
-    $.ajax({
-        url: 'https://www.omdbapi.com/?apikey=437432c&s=' + $('.input-keyword').val(),
-        success: results => {
-            const movies = results.Search;
-            let cards = ''; 
-            movies.forEach(m => {
-                cards += showCards(m);
-            });
-            $('.movie-container').html(cards);
+    const keyword = inputKeyword.value;
+    fetch('https://www.omdbapi.com/?apikey=437432c&s=' + keyword)
+        .then(response => response.json())
+        .then(response => {
+            const movies = response.Search;
+            if (movies) {
+                let cards = '';
+                movies.forEach(m => cards += showCards(m));
+                const movieContainer = document.querySelector('.movie-container');
+                movieContainer.innerHTML = cards;
 
-            // Menangani klik pada tombol Show Details
-            $('.show-details').on('click', function (event) {
-                event.preventDefault(); // Mencegah aksi default tautan
-                const imdbID = $(this).data('imdbid'); // Perbaiki pengambilan data-imdbid
-                $.ajax({
-                    url: 'https://www.omdbapi.com/?apikey=437432c&i=' + imdbID,
-                    success: m => {
-                        const movieDetail = showMovieDetail(m);
-                        $('.modal-body').html(movieDetail);
-                        $('#movieDetailModal').modal('show'); // Tampilkan modal
-                    }
+                // Menangani klik pada tombol Show Details
+                const detailButtons = document.querySelectorAll('.show-details');
+                detailButtons.forEach(btn => {
+                    btn.addEventListener('click', function (event) {
+                        event.preventDefault(); // Mencegah aksi default tautan
+                        const imdbID = this.dataset.imdbid;
+                        fetch('https://www.omdbapi.com/?apikey=437432c&i=' + imdbID)
+                            .then(response => response.json())
+                            .then(m => {
+                                const movieDetail = showMovieDetail(m);
+                                const modalBody = document.querySelector('.modal-body');
+                                modalBody.innerHTML = movieDetail;
+                                $('#movieDetailModal').modal('show'); // Tampilkan modal
+                            });
+                    });
                 });
-            });
-        },
-        error: (e) => {
-            console.log(e.responseText);
-        }
-    });
+            } else {
+                const movieContainer = document.querySelector('.movie-container');
+                movieContainer.innerHTML = '<p>No movies found</p>';
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            const movieContainer = document.querySelector('.movie-container');
+            movieContainer.innerHTML = '<p>Error fetching movies.</p>';
+        });
 }
 
 // Fungsi untuk menampilkan kartu film
@@ -71,11 +127,9 @@ function showMovieDetail(m) {
                     <li class="list-group-item"><strong>Director: </strong>${m.Director}</li>
                     <li class="list-group-item"><strong>Actors: </strong>${m.Actors}</li>
                     <li class="list-group-item"><strong>Writer: </strong>${m.Writer}</li>
-                    <li class="list-group-item"><strong>Plot: </strong> <br> ${m.Plot}</li>
+                    <li class="list-group-item"><strong>Plot: </strong><br>${m.Plot}</li>
                 </ul>
             </div>
         </div>
     </div>`;
 }
-
-
